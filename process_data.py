3b875edbe684855a9dda06f8afaa8f43826c1ef9
mkseay2 <mkseay2@illinois.edu>
2024-04-06 20:59:45 -0500

    added comments

--- process_data.py
+++ process_data.py
@@ -19,17 +19,17 @@ def calc_pitch():
             y, sr = librosa.load("data/" + genre + "/" + file_name)
             pitches, _ = librosa.piptrack(y=y, sr=sr)
 
-            # Mask non-zero elements
             nonzero_mask = (pitches != 0)
-
-            # Calculate the average for each column using masked array
+            # average the pitches over time
             column_averages = np.nanmean(np.where(nonzero_mask, pitches, np.nan), axis=0)
             pitches_processed = np.nan_to_num(column_averages, nan=0)
             
+            # grab the pitches at 0, 5, 10, ..., 100 percent
             final_pitches = []
             n = len(pitches_processed) - 1
             for percent in range(0, 105, 5):
                 ind = int(np.floor(n * (percent/100)))
+                # data is in the form [pitch, percent] where percent is the time in the audio
                 final_pitches.append([pitches_processed[ind], percent])
 
             with open('processed_data/pitch.txt', 'a') as file:
@@ -38,6 +38,7 @@ def calc_pitch():
             final_pitches = np.array(final_pitches)
             totals += final_pitches[:, 0]
 
+        # calculate the average pitch for the current genre
         totals = totals/5
         i = 0
         averages_percents = []
@@ -45,7 +46,6 @@ def calc_pitch():
             averages_percents.append([totals[i], percent])
             i += 1
 
-        print(averages_percents)
         with open('processed_data/pitch_averages.txt', 'a') as file:
             file.write(genre + ", " + str(averages_percents) + "\n")
 
@@ -62,15 +62,18 @@ def calc_volume():
 
     for genre in genres:
         file_names = os.listdir("data/" + genre + "/")
+        totals = np.zeros(21)
         
         for file_name in file_names:
             y, _ = librosa.load("data/" + genre + "/" + file_name)
             volume = librosa.feature.rms(y=y)
 
+            # grab the volumes at 0, 5, 10, ..., 100 percent
             final_volumes = []
             n = volume.shape[1] - 1
             for percent in range(0, 105, 5):
                 ind = int(np.floor(n * (percent/100)))
+                # data is in the form [volume, percent] where percent is the time in the audio
                 final_volumes.append([np.sqrt(volume[0][ind]), percent])
 
             with open('processed_data/volume.txt', 'a') as file:
@@ -79,6 +82,7 @@ def calc_volume():
             final_volumes = np.array(final_volumes)
             totals += final_volumes[:, 0]
 
+        # calculate the average volume for the current genre
         totals = totals/5
         i = 0
         averages_percents = []
@@ -91,5 +95,5 @@ def calc_volume():
 
     return
 
-calc_pitch()
+# calc_pitch()
 calc_volume()
